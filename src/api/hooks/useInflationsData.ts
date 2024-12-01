0d1ad4c6f26232d1359c17d287a484377b2b1fcb
OctaveLT <99271452+OctaveLT@users.noreply.github.com>
2024-12-01 22:16:49 +0100

    fix: data inflation json path

--- src/api/hooks/useInflationsData.ts
+++ src/api/hooks/useInflationsData.ts
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { json } from "d3";
 import { InflationsData } from "../../features/Inflations/types/data";
 
-const url = "/data/inflations.json";
+const url = "/inflations.json";
 
 function fetcher() {
     return json<InflationsData>(url);
